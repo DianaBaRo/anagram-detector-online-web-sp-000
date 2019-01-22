4c86dd3ec2f3b17660bbe5207cd78e76ad1c2606
Diana Badas <diana.badas@gmail.com>
2019-01-22 15:26:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
 
   def match(anagrams)
     anagrams.select do |word|
-      @word.split.sort == word.split.sort
+      word.split.sort == @word.split.sort
     end
     anagrams
   end
